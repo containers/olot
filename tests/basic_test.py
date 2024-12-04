a94dae86aca79c890542bec9806dfbbc7dbe22dc
tarilabs <matteo.mortari@gmail.com>
2024-12-04 08:52:01 +0100

    using gzip default level of 6 to avoid headaches
    and pinning test values

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -22,6 +22,7 @@ def test_single_file_tar(tmp_path):
 
     checksum = writer.hash_func.hexdigest()
     print(f"digest of the tar file: {checksum}")
+    assert checksum == "08e87bd00a65bebed3cabc34b4cf9a005b3bf31c25078ebd44a30fa1ad0778e3"
 
     checksum2 = get_file_hash(temp_tar_filename)
     print(f"to double-check, here is the checksum of .tar.gz re-reading it: {checksum2}")
@@ -43,15 +44,17 @@ def test_single_file_gz(tmp_path):
     
     with open(temp_tar_filename, "wb") as temp_file:
         writer = HashingWriter(temp_file)
-        with gzip.GzipFile(fileobj=writer, mode="wb", mtime=0) as gz:
+        with gzip.GzipFile(fileobj=writer, mode="wb", mtime=0, compresslevel=6) as gz:
             inner_writer = HashingWriter(gz)
             with tarfile.open(fileobj=inner_writer, mode="w") as tar:
                 tar.add(hello_path, arcname=hello_path.name)
 
     tar_checksum = inner_writer.hash_func.hexdigest()
     print(f"digest of the tar file: {tar_checksum}")
+    assert tar_checksum == "08e87bd00a65bebed3cabc34b4cf9a005b3bf31c25078ebd44a30fa1ad0778e3"
     checksum = writer.hash_func.hexdigest()
     print(f"digest of the tar.gz file: {checksum}")
+    assert checksum == "42ebad9bfd3ace9ea36c96957b41a0ab3d8f7b0528700a1a154887c459b33587"
 
     checksum2 = get_file_hash(temp_tar_filename)
     print(f"to double-check, here is the checksum of .tar.gz re-reading it: {checksum2}")
