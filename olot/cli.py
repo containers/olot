9c2c96d14b01bb6ef0dc4d2006446896e3aa8e99
tarilabs <matteo.mortari@gmail.com>
2025-02-18 13:56:43 +0100

    core: cli modelcard option, E2E test refactor
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- olot/cli.py
+++ olot/cli.py
@@ -1,3 +1,4 @@
+from os import PathLike
 from pathlib import Path
 import click
 
@@ -7,7 +8,8 @@ from .basics import oci_layers_on_top
 
 
 @click.command()
+@click.option("-m", "--modelcard", type=click.Path(exists=True, file_okay=True, dir_okay=False))
 @click.argument('ocilayout', type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click.argument('model_files', nargs=-1)
-def cli(ocilayout: str, model_files):
-    oci_layers_on_top(Path(ocilayout), model_files)
+def cli(ocilayout: str, modelcard: PathLike, model_files):
+    oci_layers_on_top(Path(ocilayout), model_files, modelcard)
