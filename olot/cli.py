c05ccee58e66405e820928b7dd63552054416f83
tarilabs <matteo.mortari@gmail.com>
2025-02-19 11:32:13 +0100

    wire logging in cli
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- olot/cli.py
+++ olot/cli.py
@@ -1,6 +1,7 @@
 from os import PathLike
 from pathlib import Path
 import click
+import logging
 
 from .basics import oci_layers_on_top
 
@@ -10,4 +11,5 @@ from .basics import oci_layers_on_top
 @click.argument('ocilayout', type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click.argument('model_files', nargs=-1)
 def cli(ocilayout: str, modelcard: PathLike, model_files):
+    logging.basicConfig(level=logging.INFO)
     oci_layers_on_top(ocilayout, model_files, modelcard)
