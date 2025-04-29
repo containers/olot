6251a71230a1f70084f7c4c86833f2b6b63a9e1d
Eric Dobroveanu <edobrove@redhat.com>
2025-04-29 10:31:58 -0400

    test: ensure dependencies are installed before running e2e tests
    Signed-off-by: Eric Dobroveanu <edobrove@redhat.com>

---
+++ e2e/deploy_kind_cluster.sh
@@ -0,0 +1,14 @@
+
+if [[ -n "$LOCAL" ]]; then
+    CLUSTER_NAME="${CLUSTER_NAME:-olot-e2e}"
+
+    echo 'Creating local Kind cluster for E2E testing...'
+
+    if [[ $(kind get clusters || false) =~ $CLUSTER_NAME ]]; then
+        echo 'Cluster already exists, skipping creation'
+
+        kubectl config use-context "kind-$CLUSTER_NAME"
+    else
+        kind create cluster -n "$CLUSTER_NAME"
+    fi
+fi
